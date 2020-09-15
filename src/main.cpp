7914d4f8fe5bb805d243ae63bba8b0011866144a
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-09-15 06:04:08 +0600

    Code formatting changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,22 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
-    if (!glfwInit())
+    if ( !glfwInit() )
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+	if ( !window )
     {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent( window );
 	
-	if (!gladLoadGL())
+	if ( !gladLoadGL() )
 	{
 		std::cout << "Can't initialize GLAD!" << "\n";
 		return -1;
@@ -30,16 +28,16 @@ int main(void)
 	
 	std::cout << "OpenGL: " << GLVersion.major << "." << GLVersion.minor << "\n";	
 	
-	glClearColor(0, 1, 0, 1);
+	glClearColor( 0, 1, 0, 1 );
 	
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while( !glfwWindowShouldClose( window ) )
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear( GL_COLOR_BUFFER_BIT );
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers( window );
 
         /* Poll for and process events */
         glfwPollEvents();
