b32a67d2688fe9f048576c8a704c7dad806c4af5
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-11-18 03:10:25 +0600

    Tank class implemented

---
+++ src/Game/Tank.cpp
@@ -0,0 +1,68 @@
+#include "Tank.h"
+#include "../Renderer/AnimatedSprite.h"
+
+Tank::Tank( std::shared_ptr<Renderer::AnimatedSprite> pSprite, float velocity, glm::vec2& position ) :
+	pSprite( std::move( pSprite ) ),
+	velocity( velocity ),
+	position( position )
+{
+	this->pSprite->SetPosition( position );
+}
+
+void Tank::Render() const
+{
+	pSprite->Render();
+}
+
+void Tank::SetOrientation( EOrientation eOrientation_in )
+{
+	if( eOrientation == eOrientation_in )
+	{
+		return;
+	}
+	eOrientation = eOrientation_in;
+	switch( eOrientation )
+	{
+	case Tank::EOrientation::Top:
+		pSprite->SetAnimation( "tankTopState" );
+		moveOffset.x = 0.f;
+		moveOffset.y = 1.f;
+		break;
+
+	case Tank::EOrientation::Bottom:
+		pSprite->SetAnimation( "tankBottomState" );
+		moveOffset.x = 0.f;
+		moveOffset.y = -1.f;
+		break;
+
+	case Tank::EOrientation::Left:
+		pSprite->SetAnimation( "tankLeftState" );
+		moveOffset.x = -1.f;
+		moveOffset.y = 0.f;
+		break;
+
+	case Tank::EOrientation::Right:
+		pSprite->SetAnimation( "tankRightState" );
+		moveOffset.x = 1.f;
+		moveOffset.y = 0.f;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Tank::Move( bool move )
+{
+	 move = move;
+}
+
+void Tank::Update( uint64_t delta )
+{
+	if( move )
+	{
+		position += delta * velocity * moveOffset;
+		pSprite->SetPosition( position );
+		pSprite->Update( delta );
+	}
+}
--- src/Game/Tank.h
+++ src/Game/Tank.h
@@ -1,6 +1,38 @@
 #pragma once
 
+#include <memory>
+
+#include <glm/vec2.hpp>
+
+namespace Renderer
+{
+	class AnimatedSprite;
+}
+
 class Tank
 {
+public:
+	enum class EOrientation
+	{
+		Top,
+		Bottom,
+		Left,
+		Right
+	};
 
+public:
+	Tank( std::shared_ptr<Renderer::AnimatedSprite> pSprite, float velocity, glm::vec2& position );
+
+	void Render() const;
+	void SetOrientation( EOrientation eOrientation_in );
+	void Move( bool move );
+	void Update( uint64_t delta );
+
+private:
+	EOrientation eOrientation = EOrientation::Top;
+	std::shared_ptr<Renderer::AnimatedSprite> pSprite;
+	bool move = false;
+	float velocity;
+	glm::vec2 position;
+	glm::vec2 moveOffset = glm::vec2( 0.f, 1.f );
 };
