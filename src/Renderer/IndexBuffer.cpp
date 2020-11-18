0a02245f4f0d263642eb21a0ad385f3432b605fa
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-11-18 19:02:25 +0600

    index buffer added & implemented

---
+++ src/Renderer/IndexBuffer.cpp
@@ -0,0 +1,35 @@
+#include "IndexBuffer.h"
+
+namespace Renderer
+{
+	IndexBuffer::~IndexBuffer()
+	{
+		glDeleteBuffers( 1, &id );
+	}
+
+	void IndexBuffer::Init( const void* data, GLuint size )
+	{
+		glGenBuffers( 1, &id );
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, id );
+		glBufferData( GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW );
+	}
+
+	void IndexBuffer::Update( const void* data, GLuint size ) const
+	{
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, id );
+		glBufferSubData( GL_ELEMENT_ARRAY_BUFFER, 0, size, data );
+	}
+
+	IndexBuffer& IndexBuffer::operator=( IndexBuffer&& vertexBuffer ) noexcept
+	{
+		id = vertexBuffer.id;
+		vertexBuffer.id = 0;
+		return *this;
+	}
+
+	IndexBuffer::IndexBuffer( IndexBuffer&& vertexBuffer ) noexcept
+	{
+		id = vertexBuffer.id;
+		vertexBuffer.id = 0;
+	}
+}
---
+++ src/Renderer/IndexBuffer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <glad/glad.h>
+
+namespace Renderer
+{
+	class IndexBuffer
+	{
+	public:
+		IndexBuffer() = default;
+		IndexBuffer( const IndexBuffer& ) = delete;
+		IndexBuffer& operator=( const IndexBuffer& ) = delete;
+		IndexBuffer& operator=( IndexBuffer&& vertexBuffer ) noexcept;
+		IndexBuffer( IndexBuffer&& vertexBuffer ) noexcept;
+		~IndexBuffer();
+
+		void Init( const void* data, GLuint size );
+		void Update( const void* data, GLuint size ) const;
+
+		inline void Bind() const { glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, id ); }
+		inline static void Unbind() { glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 ); }
+
+	private:
+		GLuint id = 0;
+	};
+}
