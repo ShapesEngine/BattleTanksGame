732a43627a67762301ecddfa907107dc370eb943
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-11-18 18:59:07 +0600

    vertex buffer implemented

--- src/Renderer/VertexBuffer.cpp
+++ src/Renderer/VertexBuffer.cpp
@@ -2,5 +2,34 @@
 
 namespace Renderer
 {
-	
+	VertexBuffer::~VertexBuffer()
+	{
+		glDeleteBuffers( 1, &id );
+	}
+
+	void VertexBuffer::Init( const void* data, GLuint size )
+	{
+		glGenBuffers( 1, &id );
+		glBindBuffer( GL_ARRAY_BUFFER, id );
+		glBufferData( GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW );
+	}
+
+	void VertexBuffer::Update( const void* data, GLuint size ) const
+	{
+		glBindBuffer( GL_ARRAY_BUFFER, id );
+		glBufferSubData( GL_ARRAY_BUFFER, 0, size, data );
+	}
+
+	VertexBuffer& VertexBuffer::operator=( VertexBuffer&& vertexBuffer ) noexcept
+	{
+		id = vertexBuffer.id;
+		vertexBuffer.id = 0;
+		return *this;
+	}
+
+	VertexBuffer::VertexBuffer( VertexBuffer&& vertexBuffer ) noexcept
+	{
+		id = vertexBuffer.id;
+		vertexBuffer.id = 0;
+	}
 }
--- src/Renderer/VertexBuffer.h
+++ src/Renderer/VertexBuffer.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <glad/glad.h>
+
 namespace Renderer
 {
 	class VertexBuffer
 	{
+	public:
+		VertexBuffer() = default;
+		VertexBuffer( const VertexBuffer& ) = delete;
+		VertexBuffer& operator=( const VertexBuffer& ) = delete;
+		VertexBuffer& operator=( VertexBuffer&& vertexBuffer ) noexcept;
+		VertexBuffer( VertexBuffer&& vertexBuffer ) noexcept;
+		~VertexBuffer();
+
+		void Init( const void* data, GLuint size );
+		void Update( const void* data, GLuint size ) const;
+
+		inline void Bind() const { glBindBuffer( GL_ARRAY_BUFFER, id ); }
+		inline static void Unbind() { glBindBuffer( GL_ARRAY_BUFFER, 0 ); }
+
+	private:
+		GLuint id = 0;
 	};
 }
