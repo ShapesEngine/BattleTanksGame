0b933ef9ce25ce5928ebb066535e74bacf597bd3
Yernar Aldabergenov <yernar.aa@gmail.com>
2020-12-20 02:59:55 +0600

    vertexbuflayout warning fix

--- src/System/Utils/VertexBufferLayout.cpp
+++ src/System/Utils/VertexBufferLayout.cpp
@@ -4,7 +4,7 @@ namespace Utils
 {
 	void VertexBufferLayout::AddElementLayoutFloat( GLuint count, bool normalized )
 	{
-		layoutElments.push_back( { (GLint)count, GL_FLOAT, normalized, count * sizeof( GLfloat ) } );
+		layoutElments.push_back( { (GLint)count, GL_FLOAT, normalized, (GLuint)count * sizeof( GLfloat ) } );
 		stride += layoutElments.back().size;
 	}
 }
